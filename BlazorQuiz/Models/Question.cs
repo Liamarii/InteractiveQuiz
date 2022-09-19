6b4db4501f992fda1adc2e0f082a660b73363f90
Liamarii <39436443+Liamarii@users.noreply.github.com>
2022-09-19 15:49:08 +0100

    - Created the new quiz page - Updated the navigation menu - Created the question model - Attached some quiz css

---
+++ BlazorQuiz/Models/Question.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorQuiz.Models
+{
+    public class Question
+    {
+        public string Content { get; set; } = string.Empty;
+        public IEnumerable<string> Answers { get; set; } = new List<string>();
+        public string Answer { get; set; } = string.Empty;
+    }
+}
---
+++ BlazorQuiz/Pages/QuizBase.cs
@@ -0,0 +1,40 @@
+﻿using BlazorQuiz.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorQuiz.Pages
+{
+    public class QuizBase : ComponentBase
+    {
+        public List<Question> Questions { get; set; } = new List<Question>();
+
+        protected override Task OnInitializedAsync()
+        {
+            LoadQuestions();
+            return base.OnInitializedAsync();
+        }
+
+        private void LoadQuestions()
+        {
+            Question q1 = new()
+            {
+                Content = "They don't think it be like it be..",
+                Answers = new List<string>() { "But do?", "So I do", "But it do" },
+                Answer = "But it do"
+            };
+            Question q2 = new()
+            {
+                Content = "I push my fingers..",
+                Answers = new List<string>() { "Into a grapefruit", "Into my eyes", "Into my pocket" },
+                Answer = "Into my eyes"
+            };
+            Question q3 = new()
+            {
+                Content = "Where is the elephants foot?",
+                Answers = new List<string>() { "On the elephants leg", "Russia", "Africa" },
+                Answer = "Russia"
+            };
+            Questions.AddRange(new List<Question>{ q1, q2, q3 });
+
+        }
+    }
+}
