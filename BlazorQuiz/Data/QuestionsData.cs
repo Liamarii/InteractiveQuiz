e4345250c1a3326c2cc8d1948d5bafc9dad1b2ae
Liamarii <39436443+Liamarii@users.noreply.github.com>
2022-09-24 18:04:16 +0100

    Move the quiz to the index page since it was a pointless default Moved some of the hard coded bits into the C# Made it so different quizes can be set but this needs setting up still in the HTML

---
+++ BlazorQuiz/Data/QuestionsData.cs
@@ -0,0 +1,51 @@
+﻿using BlazorQuiz.Models;
+
+namespace BlazorQuiz.Data
+{
+    public class QuestionsData
+    {
+        public static Tuple<string, List<Question>> GetQuestionSet(QuestionSet questionSet)
+        {
+            return questionSet switch
+            {
+                _ => HalloweenQuestionSet()
+            };
+        }
+
+        public enum QuestionSet
+        {
+            Halloween
+        }
+
+        private static Tuple<string, List<Question>> HalloweenQuestionSet()
+        {
+            return Tuple.Create("🎃 Halloween quiz", new List<Question>()
+            {
+                new()
+                {
+                    Content = "Who plotted to blow up the House of Lords?",
+                    Answers = new List<string>() { "Guy Fieri", "Guy Fawkes", "Mr Walnut" },
+                    CorrectAnswer = "Guy Fawkes"
+                },
+                new()
+                {
+                    Content = "What was candy corn originally called?",
+                    Answers = new List<string>() { "Chicken Feed", "Dog Eggs", "Goat Seed", "Mr Walnut" },
+                    CorrectAnswer = "Chicken Feed"
+                },
+                new()
+                {
+                    Content = "What was the most jack o'lanterns on display at once?",
+                    Answers = new List<string>() { "30,581", "4", "8 billion" },
+                    CorrectAnswer = "80,581"
+                },
+                new()
+                {
+                    Content = "When is Halloween?",
+                    Answers = new List<string>() { "Easter", "Every year", "Boctober" },
+                    CorrectAnswer = "Every year"
+                }
+            });
+        }
+    };
+}
